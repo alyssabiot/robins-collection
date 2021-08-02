4c291462187964975f34de13e6e7ed887ca6ca8a
Alyssa Biot <alyssa.biot@gmail.com>
2021-08-02 23:36:02 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+40.times do |i|
+  Figurine.create(name: "personnage n#{i+1}")
+end
