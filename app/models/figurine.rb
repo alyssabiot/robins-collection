928b999f8c484acaae54a14d8f1ffd50bd9c19cc
Alyssa Biot <alyssa.biot@gmail.com>
2021-08-02 12:13:55 +0200

    add portrait and refacto picture display

--- app/models/figurine.rb
+++ app/models/figurine.rb
@@ -2,9 +2,11 @@ class Figurine < ApplicationRecord
   validates :name, presence: true
   validate :acceptable_reference_picture
   validate :acceptable_painted_picture
+  validate :acceptable_portrait_picture
 
   has_one_attached :reference_picture
   has_one_attached :painted_picture
+  has_one_attached :portrait
 
   # TO DO : refacto the image validations
 
@@ -31,6 +33,19 @@ class Figurine < ApplicationRecord
     acceptable_types = ["image/jpeg", "image/png"]
     unless acceptable_types.include?(painted_picture.content_type)
       errors.add(:painted_picture, "doit être un JPEG ou PNG")
+    end
+  end
+
+  def acceptable_portrait
+    return unless portrait.attached?
+
+    unless portrait.byte_size <= 1.megabyte
+      errors.add(:portrait, "doit faire moins de 1Mb")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(portrait.content_type)
+      errors.add(:portrait, "doit être un JPEG ou PNG")
     end
   end
 end
