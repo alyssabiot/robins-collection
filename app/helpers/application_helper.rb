ffbccf9a721ab3b41d231fffa438966bb6b794f4
Alyssa Biot <alyssa.biot@gmail.com>
2021-08-04 13:57:54 +0200

    fix url after ordering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,10 @@ module ApplicationHelper
     value ? "<i class='fa fa-check green'></i>".html_safe : "<i class='fa fa-times red'></i>".html_safe
   end
 
-  def sortable(column, title = nil)
-    title ||= column.titleize
+  def sortable(column, title, path)
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to title, figurines_path(sort: column, direction: direction), class: css_class
   end
 
   def display_order_arrow(column)
@@ -24,5 +23,5 @@ module ApplicationHelper
     else
       "#{column} <i class='fa fa-angle-up'></i>".html_safe
     end
-  end 
+  end
 end
