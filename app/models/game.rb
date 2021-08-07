4e2ebe80ad02497e25baa6573b61040e66c3237d
Alyssa Biot <alyssa.biot@gmail.com>
2021-08-07 13:30:14 +0200

    add filter for available games

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,9 @@ class Game < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :figurines, through: :bookings
 
+  # TO DO : refacto
+  scope :available_for_figurine, ->(figurine) { Game.select { |g| !g.figurines.include?(figurine) } }
+
   def name_with_date
     game_date ? "#{name} - #{game_date.strftime('%d/%m/%Y')}" : name
   end
